b9279ecef25e6195a1de4873361942d24600a703
Pranava S Balugari <stalin.pranava@gmail.com>
2017-03-31 00:26:44 +1100

    Move articles from main layout to app-article-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Article } from './article';
-
 
 @Component({
   selector: 'app-root',
@@ -8,24 +6,8 @@ import { Article } from './article';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  articles: Article[];
 
   constructor() {
-    this.articles = [
-      new Article('pede. Cum sociis natoque penatibus et magnis dis parturient montes,', 'Fusce', 10),
-      new Article('ut erat. Sed nunc est, mollis non, cursus non, egestas', 'Fusce aliquam, enim nec tempus sdales,'),
-      new Article('in faucibus orci luctus et ultrices posuere cubilia Curae; Donec', 'eros non enim commodon'),
-      new Article('morbi tristique senectus et netus et malesuada fames ac turpis', 'Proin'),
-      new Article('ante dictum mi, ac mattis velit justo nec ante. Maecenas', 'lectue'),
-      new Article('dictum mi, ac mattis velit justo nec ante. Maecenas mi', 'estrum urna,'),
-      new Article('Aliquam nisl. Nulla eu neque pellentesque massa lobortis ultibh sit amet', 'blah blah'),
-      new Article('massa rutrum magna. Cras convallis convallis dolor. Quisque tincic sem', 'blah blah'),
-      new Article('eu tempor erat neque non quam. Pellentesque habitant mt netus', 'blah blah'),
-      new Article('Praesent eu nulla at sem molestie sodales. Mauris blandit enim', 'eurabitur'),
-      new Article('dolor. Fusce mi lorem, vehicula et, rutrum eu, ultrices sit', 'massa. Mauris'),
-      new Article('natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'blandit.'),
-      new Article('ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et,', 'non mam et,'),
-      new Article('eu elit. Nulla facilisi. Sed neque. Sed eget lacus. Mauris', 'Nam'),
-    ];
+
   }
 }
--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../article';
 
 @Component({
   selector: 'app-article-list',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent implements OnInit {
+  articles: Article[];
 
   constructor() { }
 
   ngOnInit() {
+    this.articles = [
+      new Article('pede. Cum sociis natoque penatibus et magnis dis parturient montes,', 'Fusce', 10),
+      new Article('ut erat. Sed nunc est, mollis non, cursus non, egestas', 'Fusce aliquam, enim nec tempus sdales,'),
+      new Article('in faucibus orci luctus et ultrices posuere cubilia Curae; Donec', 'eros non enim commodon'),
+      new Article('morbi tristique senectus et netus et malesuada fames ac turpis', 'Proin'),
+      new Article('ante dictum mi, ac mattis velit justo nec ante. Maecenas', 'lectue'),
+      new Article('dictum mi, ac mattis velit justo nec ante. Maecenas mi', 'estrum urna,'),
+      new Article('Aliquam nisl. Nulla eu neque pellentesque massa lobortis ultibh sit amet', 'blah blah'),
+      new Article('massa rutrum magna. Cras convallis convallis dolor. Quisque tincic sem', 'blah blah'),
+      new Article('eu tempor erat neque non quam. Pellentesque habitant mt netus', 'blah blah'),
+      new Article('Praesent eu nulla at sem molestie sodales. Mauris blandit enim', 'eurabitur'),
+      new Article('dolor. Fusce mi lorem, vehicula et, rutrum eu, ultrices sit', 'massa. Mauris'),
+      new Article('natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'blandit.'),
+      new Article('ante. Nunc mauris sapien, cursus in, hendrerit consectetuer, cursus et,', 'non mam et,'),
+      new Article('eu elit. Nulla facilisi. Sed neque. Sed eget lacus. Mauris', 'Nam'),
+    ];
   }
 
 }
